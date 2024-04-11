b49d19bee0beec55aa6e110a02b42e07b42f78d2
Taehun88 <taehun8235@gmail.com>
2024-04-11 20:07:23 +0900

    IdTable add

---
+++ src/main/java/com/plog/demo/model/IdTable.java
@@ -0,0 +1,9 @@
+package com.plog.demo.model;
+
+public class IdTable {
+    private String id;
+    private String name;
+    private String phoneNum;
+    private String email;
+    private String nickname;
+}
