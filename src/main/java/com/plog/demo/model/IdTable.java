c6caf0c1f6c9f9ea5ffde9b995030d9b4e3b21a3
Taehun88 <taehun8235@gmail.com>
2024-04-13 21:17:20 +0900

    database connect & database test

--- src/main/java/com/plog/demo/model/IdTable.java
+++ src/main/java/com/plog/demo/model/IdTable.java
@@ -1,13 +1,37 @@
 package com.plog.demo.model;
 
-public class IdTable {
-    private String id;
-    private String name;
-    private String phoneNum;
-    private String email;
-    private String nickname;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    String getId() {
-        return id;
+@Data
+@Entity
+@NoArgsConstructor
+public class IdTable {
+    @Id
+    private String id;
+    @Column
+    private String name;
+    @Column
+    private String password;
+    @Column
+    private String email;
+    @Column
+    private String phonenum;
+    @Column
+    private String nickname;
+    @Column
+    private String role;
+
+    public IdTable(String id, String name, String password, String email, String phonenum, String nickname, String role) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.phonenum = phonenum;
+        this.nickname = nickname;
+        this.role = role;
     }
 }
