15ad54c5e5de403fab838af3c9dafc2030956f45
myeongha <audgkrnt@naver.com>
2024-04-15 20:41:21 +0900

    sign/feat

--- src/main/java/com/plog/demo/Dto/UserDto.java
+++ src/main/java/com/plog/demo/Dto/UserDto.java
@@ -1,4 +1,4 @@
-package com.plog.demo.Dto;
+package com.plog.demo.dto;
 
 import com.plog.demo.model.IdTable;
 
@@ -9,19 +9,14 @@ public class UserDto {
     private String email;
     private String phonenum;
     private String nickname;
-    private String role;
 
-    public UserDto(String id, String name, String password, String email, String phonenum, String nickname, String role) {
+    public UserDto(String id, String name, String password, String email, String phonenum, String nickname) {
         this.id = id;
         this.name = name;
         this.password = password;
         this.email = email;
         this.phonenum = phonenum;
         this.nickname = nickname;
-        this.role = role;
     }
 
-    public IdTable toEntity() {
-        return new IdTable(id, name, password, email, phonenum, nickname, role);
-    }
 }
