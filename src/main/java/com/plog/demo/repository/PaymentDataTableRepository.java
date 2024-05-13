a219dd81c4717a08cd8be69f888aa555af4a387e
myeongha <audgkrnt@naver.com>
2024-05-13 15:32:27 +0900

    modify

--- src/main/java/com/plog/demo/repository/PaymentDataTableRepository.java
+++ src/main/java/com/plog/demo/repository/PaymentDataTableRepository.java
@@ -3,8 +3,10 @@ package com.plog.demo.repository;
 import com.plog.demo.model.IdTable;
 import com.plog.demo.model.PaymentDataTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 
 public interface PaymentDataTableRepository extends JpaRepository<PaymentDataTable, Integer> {
 
