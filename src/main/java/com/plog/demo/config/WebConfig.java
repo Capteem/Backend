fc57e1c25ced7880c91b320ee71533e1ceb54ea3
myeongha <audgkrnt@naver.com>
2024-05-19 15:01:22 +0900

    fix/portfolio

---
+++ src/main/java/com/plog/demo/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.plog.demo.config;
+
+import com.plog.demo.config.interceptor.LogInInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//c
+@Configuration
+@EnableWebMvc
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LogInInterceptor logInInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInInterceptor)
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/sign-api/signin", "/sign-api/signup", "/sign-api/refresh");
+//    }
+}
---
+++ src/main/java/com/plog/demo/config/interceptor/LogInInterceptor.java
@@ -0,0 +1,32 @@
+package com.plog.demo.config.interceptor;
+
+import com.plog.demo.config.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class LogInInterceptor implements HandlerInterceptor {
+
+    final private JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        log.info("[LogInInterceptor] 인터셉터 실행");
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if(token == null || !jwtTokenProvider.validationToken(token)) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+
+        return true;
+    }
+}
