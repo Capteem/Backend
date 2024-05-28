ce9402a60214e39fa384cb9093741c75615fb6b7
Taehun88 <taehun8235@gmail.com>
2024-05-28 14:09:23 +0900

    add option method to webconfig

--- src/main/java/com/plog/demo/config/WebConfig.java
+++ src/main/java/com/plog/demo/config/WebConfig.java
@@ -18,10 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("https://picturewithlog.com:8084")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://picturewithlog.com:8084", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
     @Override
